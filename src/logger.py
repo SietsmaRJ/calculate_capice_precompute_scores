b726234ff2e8fb9b9430189e8e529317031c7c55
rjsietsma <r.j.sietsma@st.hanze.nl>
2020-05-13 11:16:47 +0200

    Further bug fixing in logger.py

--- src/logger.py
+++ src/logger.py
@@ -12,26 +12,23 @@ class Logger:
             self.output_dir = None
             self.logfile = None
             self.utilities = Utilities()
-            self.check_if_dir_exist()
-            self.check_if_log_file_exist()
 
         def set_output_dir(self, output_loc):
             self.output_dir = output_loc
+            self._check_if_dir_exist()
+            self._check_if_log_file_exist()
 
-        def check_if_dir_exist(self):
+        def _check_if_dir_exist(self):
             output_dir = os.path.join(self.output_dir, 'log_output')
             self.utilities.check_if_dir_exists(output_dir)
             self.output_dir = output_dir
 
-        def check_if_log_file_exist(self):
+        def _check_if_log_file_exist(self):
             log_file_name = '{}_logfile.txt'.format(datetime.now().strftime(
                 "%Y_%m_%d_%H%M%S_%f"))
             joined_path = os.path.join(self.output_dir, log_file_name)
             self.utilities.check_if_file_exists(joined_path)
             self.logfile = joined_path
-
-        def get_output_dir(self):
-            return self.output_dir
 
         def log(self, message):
             timestamp = datetime.now().strftime("%H:%M:%S_%f")
